bd347678af0c04e9877e9d2744a0e26c59328f37
eliww2 <eliww2@illinois.edu>
2021-03-31 19:07:39 -0500

    Final commit.

--- include/cs126linkedlist/ll.hpp
+++ include/cs126linkedlist/ll.hpp
@@ -8,6 +8,7 @@
 
 namespace cs126linkedlist {
 
+// Default Constructor
 template <typename ElementType>
 LinkedList<ElementType>::LinkedList() {
   head->element = NULL;
@@ -15,6 +16,7 @@ LinkedList<ElementType>::LinkedList() {
   head->previousItem = nullptr;
 }
 
+// Vector to List constructor
 template <typename ElementType>
 LinkedList<ElementType>::LinkedList(const std::vector<ElementType>& values) {
   Node *previous = head;
@@ -73,38 +75,100 @@ LinkedList<ElementType>::LinkedList(LinkedList<ElementType>&& source) noexcept {
 template <typename ElementType>
 LinkedList<ElementType>::~LinkedList() {
   clear();
+  delete head;
 }
 
 // Copy assignment operator
 template <typename ElementType>
 LinkedList<ElementType>& LinkedList<ElementType>::operator=(
     const LinkedList<ElementType>& source) {
+  Node *previous = head;
+  head->previousItem = nullptr;
+  head->element = source.head->element;
+  if (source.size() <= 1) {
+    head->nextItem = nullptr;
+  } else {
+    Node *source_node = source.head->nextItem;
+    for (size_t i = 1; i < source.size(); i++) {
+      Node* current_node = new Node();
+      previous->nextItem = current_node;
+      current_node->element = source_node->element;
+      current_node->previousItem = previous;
+      previous = current_node;
+      if (i + 1 == source.size()) {
+        current_node->nextItem = nullptr;
+      }
+      source_node = source_node->nextItem;
+      previous = current_node;
+
+    }
+  }
+  return *this;
 }
 
 // Move assignment operator
 template <typename ElementType>
 LinkedList<ElementType>& LinkedList<ElementType>::operator=(
-    LinkedList<ElementType>&& source) noexcept {}
-
-template <typename ElementType>
-void LinkedList<ElementType>::push_front(const ElementType& value) {}
-
-template <typename ElementType>
-void LinkedList<ElementType>::push_back(const ElementType& value) {}
-
-template <typename ElementType>
-ElementType LinkedList<ElementType>::front() const {}
-
-template <typename ElementType>
-ElementType LinkedList<ElementType>::back() const {}
-
-template <typename ElementType>
-void LinkedList<ElementType>::pop_front() {
-
+    LinkedList<ElementType>&& source) noexcept {
+  head = source.head;
+  source.head = nullptr;
+  return *this;
 }
 
 template <typename ElementType>
-void LinkedList<ElementType>::pop_back() {}
+void LinkedList<ElementType>::push_front(const ElementType& value) {
+  Node *new_node = new Node();
+  new_node->nextItem = head;
+  new_node->previousItem = nullptr;
+  new_node->element = value;
+  head->previousItem = new_node;
+  head = new_node;
+}
+
+template <typename ElementType>
+void LinkedList<ElementType>::push_back(const ElementType& value) {
+    Node *new_node = new Node();
+    new_node->nextItem = nullptr;
+    new_node->element = value;
+    Node *current_node = head;
+    while (current_node->nextItem != nullptr) {
+      current_node = current_node->nextItem;
+    }
+    current_node->nextItem = new_node;
+    new_node->previousItem = current_node;
+}
+
+template <typename ElementType>
+ElementType LinkedList<ElementType>::front() const {
+  return head->element;
+}
+
+template <typename ElementType>
+ElementType LinkedList<ElementType>::back() const {
+  Node *current_node = head;
+  while (current_node->nextItem != nullptr) {
+    current_node = current_node->nextItem;
+  }
+  return current_node->element;
+}
+
+template <typename ElementType>
+void LinkedList<ElementType>::pop_front() {
+  Node *next_node = head->nextItem;
+  next_node->previousItem = nullptr;
+  delete head;
+  head = next_node;
+}
+
+template <typename ElementType>
+void LinkedList<ElementType>::pop_back() {
+  Node *current_node = head;
+  while (current_node->nextItem != nullptr) {
+    current_node = current_node->nextItem;
+  }
+  current_node->previousItem->nextItem = nullptr;
+  delete current_node;
+}
 
 template <typename ElementType>
 size_t LinkedList<ElementType>::size_helper(Node *node) const{
@@ -116,6 +180,9 @@ size_t LinkedList<ElementType>::size_helper(Node *node) const{
 
 template <typename ElementType>
 size_t LinkedList<ElementType>::size() const {
+  if (head == nullptr) {
+    return 0;
+  }
   if (head->nextItem == nullptr) {
     if (head->element != NULL) {
       return 1;
@@ -126,54 +193,148 @@ size_t LinkedList<ElementType>::size() const {
 }
 
 template <typename ElementType>
-bool LinkedList<ElementType>::empty() const {}
+bool LinkedList<ElementType>::empty() const {
+  if (size() == 0) {
+    return true;
+  } else if (size() == 1 && head->element == NULL) {
+    return true;
+  }
+  Node *current_node = head;
+  while(current_node->nextItem != nullptr) {
+    if (current_node->element != NULL) {
+      return false;
+    }
+    current_node = current_node->nextItem;
+  }
+  return true;
+}
 
 template <typename ElementType>
 void LinkedList<ElementType>::clear() {
-
+  if (size() > 1) {
+    Node *current_node = head->nextItem;
+    head->nextItem = nullptr;
+    while (current_node->nextItem != nullptr) {
+      current_node = current_node->nextItem;
+      delete current_node->previousItem;
+    }
+    delete current_node;
+  }
+  head->element = NULL;
 }
 
 template <typename ElementType>
 std::ostream& operator<<(std::ostream& os,
-                         const LinkedList<ElementType>& list) {}
+                         const LinkedList<ElementType>& list) {
+  cs126linkedlist::LinkedList<ElementType>::const_iterator iter = list.begin();
+  for (size_t i = 0; i < list.size(); i++) {
+    std::cout << iter.operator*();
+    if(i + 1 == list.size()) {
+      std::cout << ".";
+    } else {
+      std::cout << ", ";
+    }
+    *iter.operator++();
+  }
+  return os;
+}
 
 template <typename ElementType>
-void LinkedList<ElementType>::RemoveNth(size_t n) {}
+void LinkedList<ElementType>::RemoveNth(size_t n) {
+  if (n == 0) {
+    pop_front();
+    return;
+  } else if (n == size() - 1){
+    pop_back();
+    return;
+  }
+  Node *current_node = head;
+  for (size_t i = 0; i < n; i++) {
+    current_node = current_node->nextItem;
+  }
+  current_node->previousItem->nextItem = current_node->nextItem;
+  current_node->nextItem->previousItem = current_node->previousItem;
+  delete current_node;
+}
 
 template <typename ElementType>
-void LinkedList<ElementType>::RemoveOdd() {}
+void LinkedList<ElementType>::RemoveOdd() {
+  for (size_t i = 1; i < size(); i++) {
+    RemoveNth(i);
+  }
+}
 
 template <typename ElementType>
 bool LinkedList<ElementType>::operator==(
-    const LinkedList<ElementType>& rhs) const {}
+    const LinkedList<ElementType>& rhs) const {
+  Node *current_node = head;
+  Node *current_rhs = rhs.head;
+  if (size() != rhs.size()) {
+    return false;
+  }
+  for (size_t i = 0; i < size(); i++) {
+    if (current_node->element != current_rhs->element) {
+      return false;
+    }
+    if (i + 1 != size()) {
+      current_node = current_node->nextItem;
+      current_rhs = current_rhs->nextItem;
+    } else {
+      return true;
+    }
+  }
+  return false;
+}
 
 template <typename ElementType>
 bool LinkedList<ElementType>::operator!=(
-    const LinkedList<ElementType>& rhs) const {}
+    const LinkedList<ElementType>& rhs) const {
+  if (*this == rhs) {
+    return false;
+  }
+  return true;
+}
 
 template <typename ElementType>
-typename LinkedList<ElementType>::iterator& LinkedList<ElementType>::iterator::
-operator++() {}
+typename LinkedList<ElementType>::iterator& LinkedList<ElementType>::iterator::operator++() {
+  current_ = current_->nextItem;
+  return *this;
+}
 
 template <typename ElementType>
-ElementType& LinkedList<ElementType>::iterator::operator*() const {}
+ElementType& LinkedList<ElementType>::iterator::operator*() const {
+  return current_->element;
+}
 
 template <typename ElementType>
 bool LinkedList<ElementType>::iterator::operator!=(
     const typename LinkedList<ElementType>::iterator& other) const {}
 
 template <typename ElementType>
-typename LinkedList<ElementType>::iterator LinkedList<ElementType>::begin() {}
+typename LinkedList<ElementType>::iterator LinkedList<ElementType>::begin() {
+  return iterator(this->head);
+}
 
 template <typename ElementType>
-typename LinkedList<ElementType>::iterator LinkedList<ElementType>::end() {}
+typename LinkedList<ElementType>::iterator LinkedList<ElementType>::end() {
+  auto my_iterator = this->begin();
+  for (size_t i = 1; i < size(); i++) {
+    my_iterator++;
+  }
+  return my_iterator;
+}
 
 template <typename ElementType>
 typename LinkedList<ElementType>::const_iterator&
-LinkedList<ElementType>::const_iterator::operator++() {}
+LinkedList<ElementType>::const_iterator::operator++() {
+  current_ = current_->nextItem;
+  return *this;
+}
 
 template <typename ElementType>
-const ElementType& LinkedList<ElementType>::const_iterator::operator*() const {}
+const ElementType& LinkedList<ElementType>::const_iterator::operator*() const {
+  return current_->element;
+}
 
 template <typename ElementType>
 bool LinkedList<ElementType>::const_iterator::operator!=(
@@ -181,10 +342,18 @@ bool LinkedList<ElementType>::const_iterator::operator!=(
 
 template <typename ElementType>
 typename LinkedList<ElementType>::const_iterator
-LinkedList<ElementType>::begin() const {}
+LinkedList<ElementType>::begin() const {
+  return const_iterator(this->head);
+}
 
 template <typename ElementType>
 typename LinkedList<ElementType>::const_iterator LinkedList<ElementType>::end()
-    const {}
+    const {
+  Node *current_node = this->head;
+  for (int i = 0; i < size(); i++) {
+    current_node = current_node->nextItem;
+  }
+  return iterator(current_node);
+}
 
 }  // namespace cs126linkedlist
--- tests/test_ll.cc
+++ tests/test_ll.cc
@@ -8,9 +8,11 @@
 
 using cs126linkedlist::LinkedList;
 
-// Read more on SECTIONs here:
-// `https://github.com/catchorg/Catch2/tree/master/docs`
-// in the "Test Cases and Sections" file.
+TEST_CASE("Size") {
+  const std::vector<int> &values = {1, 2, 3, 4, 5};
+  auto *list = new LinkedList<int>(values);
+  REQUIRE(list->size() == 5);
+}
 
 TEST_CASE("Constructors") {
   const std::vector<int> &values = {1, 2, 3, 4, 5};
@@ -23,39 +25,168 @@ TEST_CASE("Constructors") {
   SECTION("Constructor using vector") {
     auto *list = new LinkedList<int>(values);
     REQUIRE(list->size() == 5);
-    REQUIRE(list->size() != 7);
+    REQUIRE(list->begin().operator*() == 1);
   }
 
   SECTION("Constructor using another list") {
     const auto *source = new LinkedList<int>(values);
     auto list(source);
+    REQUIRE(list->begin().operator*() == 1);
     REQUIRE(list->size() == 5);
-    REQUIRE(source->size() == 5);
   }
 
   SECTION("Move constructor") {
     const auto source = new LinkedList<int>(values);
-    REQUIRE(source->size() == 5);
     LinkedList<int> list(std::move(*source));
+    REQUIRE(source->size() == 0);
     REQUIRE(list.size() == 5);
   }
+}
 
-}/*
-TEST_CASE("Push Back", "[constructor][push_back][size][empty]") {
-  LinkedList<int> list;
+TEST_CASE("Assignment Operators") {
+  const std::vector<int> &values = {1, 2, 3, 4, 5};
 
-  REQUIRE(list.size() == 0);
-  REQUIRE(list.empty());
-
-  SECTION("Push back one element") {
-    list.push_back(1);
-    REQUIRE(list.size() == 1);
+  SECTION("Copy Assignment Operator") {
+    LinkedList<int> list;
+    auto *source = new LinkedList<int>(values);
+    list = *source;
+    REQUIRE(list.size() == 5);
+    list.pop_front();
+    REQUIRE(source->size() == 5);
+    REQUIRE(list.size() == 4);
   }
 
-  SECTION("Push back two elements") {
-    list.push_back(-1);
-    list.push_back(10000);
-    REQUIRE(list.size() == 2);
+  SECTION("Move Assignment Operator") {
+    LinkedList<int> list;
+    auto *source = new LinkedList<int>(values);
+    list = std::move(*source);
+    REQUIRE(list.size() == 5);
+    REQUIRE(source->size() == 0);
   }
 }
-*/
+
+TEST_CASE("Insertion operators") {
+  const std::vector<int> &values = {1, 2, 3, 4, 5};
+
+  SECTION("Push front") {
+    auto *list = new LinkedList<int>(values);
+    list->push_front(9);
+    REQUIRE(list->begin().operator*() == 9);
+  }
+
+  SECTION("Push back") {
+    auto *list = new LinkedList<int>(values);
+    list->push_back(9);
+    auto my_it = list->begin();
+    int i = 0;
+    while(i < 5) {
+      my_it.operator++();
+      i++;
+    }
+    REQUIRE(my_it.operator*() == 9);
+  }
+
+}
+
+TEST_CASE("Deletion operators") {
+  const std::vector<int> &values = {1, 2, 3, 4, 5};
+
+  SECTION("Pop front") {
+    auto *source = new LinkedList<int>(values);
+    source->pop_front();
+    REQUIRE(source->size() == 4);
+    REQUIRE(source->begin().operator*() == 2);
+  }
+  SECTION("Pop back") {
+    auto *source = new LinkedList<int>(values);
+    source->pop_back();
+    REQUIRE(source->size() == 4);
+    REQUIRE(source->begin().operator*() == 1);
+  }
+
+  SECTION("RemoveNth") {
+    auto *source = new LinkedList<int>(values);
+    source->RemoveNth(1);
+    REQUIRE(source->begin().operator++().operator*() == 3);
+  }
+
+  SECTION("RemoveOdd") {
+    auto *source = new LinkedList<int>(values);
+    REQUIRE(source->size() == 5);
+    source->RemoveOdd();
+    REQUIRE(source->size() == 3);
+    REQUIRE(source->begin().operator++().operator*() == 3);
+  }
+}
+
+TEST_CASE("Front and Back") {
+  const std::vector<int> &values = {1, 2, 3, 4, 5};
+
+  SECTION("Front") {
+    auto *list = new LinkedList<int>(values);
+    REQUIRE(list->front() == 1);
+  }
+
+  SECTION("Back") {
+    auto *list = new LinkedList<int>(values);
+    REQUIRE(list->back() == 5);
+  }
+
+}
+
+TEST_CASE("Override operators") {
+  const std::vector<int> &values = {1, 2, 3, 4, 5};
+  const std::vector<int> &other = {1, 2, 3, 4, 4};
+
+  SECTION("operator==") {
+    auto *list = new LinkedList<int>(values);
+    auto *different = new LinkedList<int>(other);
+    auto *same = new LinkedList<int>(values);
+    REQUIRE(*list == *same);
+    REQUIRE(!(*list == *different));
+  }
+
+  SECTION("operator!=") {
+    auto *list = new LinkedList<int>(values);
+    auto *different = new LinkedList<int>(other);
+    auto *same = new LinkedList<int>(values);
+    REQUIRE(!(*list != *same));
+    REQUIRE(*list != *different);
+  }
+
+}
+
+TEST_CASE("Iterator operators") {
+  const std::vector<int> &values = {1, 2, 3, 4, 5};
+
+  SECTION("Iterator++") {
+    auto *list = new LinkedList<int>(values);
+    auto my_it = list->begin();
+    auto num = my_it.operator*();
+    my_it.operator++();
+    REQUIRE(my_it.operator*() != num);
+  }
+
+  SECTION("operator*") {
+    auto *list = new LinkedList<int>(values);
+    REQUIRE(list->begin().operator*() == 1);
+  }
+
+  SECTION("Begin") {
+    auto *list = new LinkedList<int>(values);
+    REQUIRE(list->begin().operator*() == 1);
+    REQUIRE(list->begin().operator++().operator*() == 2);
+  }
+
+  SECTION("End") {
+    auto *list = new LinkedList<int>(values);
+    REQUIRE(!(list->begin().operator*() == 5));
+  }
+
+}
+
+
+
+
+
+
